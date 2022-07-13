0aaf462115f64e60aa6946a809dd16fb0a679219
EugeBr <eugebordenave@gmail.com>
2022-07-13 16:31:20 +0200

    users routes

--- profile-app-server/routes/index.routes.js
+++ profile-app-server/routes/index.routes.js
@@ -7,4 +7,7 @@ router.get("/", (req, res, next) => {
 const authRoutes = require("./auth.routes")
 router.use("/auth", authRoutes)
 
+const usersRoutes = require("./users.routes")
+router.use("/users", usersRoutes)
+
 module.exports = router;
---
+++ profile-app-server/routes/users.routes.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router()
+const User = require("../models/User.model")
+
+router.put("/", async (req, res, next) => {
+	try {
+		const id = req.session.currentUser._id;
+		const image = req.body;
+		const user = await User.findByIdAndUpdate(id, image, { new: true });
+		return res.status(200).json(user)
+	} catch (error) {
+		next(error)
+	}
+})
+
+router.get("/", async (req, res, next) => {
+    try {
+        const id = req.session.currentUser._id;
+        const user = await User.findById(id)
+        return res.status(200).json(user)
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
