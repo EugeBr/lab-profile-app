6d5dbf9ce875d0285136f90870f691492c466e9d
EugeBr <eugebordenave@gmail.com>
2022-07-13 16:55:25 +0200

    upload route

--- profile-app-server/routes/users.routes.js
+++ profile-app-server/routes/users.routes.js
@@ -1,16 +1,27 @@
 const express = require("express")
 const router = express.Router()
 const User = require("../models/User.model")
+const isAuthenticated = require("../middlewares/isAuthenticated")
 
 router.put("/", async (req, res, next) => {
-	try {
-		const id = req.session.currentUser._id;
-		const image = req.body;
-		const user = await User.findByIdAndUpdate(id, image, { new: true });
-		return res.status(200).json(user)
-	} catch (error) {
-		next(error)
-	}
+    try {
+        const id = req.session.currentUser._id;
+        const image = req.body;
+        const user = await User.findByIdAndUpdate(id, image, { new: true });
+        return res.status(200).json(user)
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.post("/upload", isAuthenticated, async (req, res, next) => {
+    try {
+        const { image } = req.body
+        await User.create({ image })
+        return res.status(200).json(image)
+    } catch (error) {
+        next(error)
+    }
 })
 
 router.get("/", async (req, res, next) => {
